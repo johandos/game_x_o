347a972c0897177e355b53b13c88bcd84a4a1b17
johan <johan.ospina05@gmail.com>
2022-11-16 22:24:56 +0100

    - Implementar y configurar dotenv - Falta realizar test - Falta READ.md - Falta agregar logs, errores y revisar la validacion de ganador - Se agrega .env por pruebas pero no deberia ir en el git

--- index.php
+++ index.php
@@ -5,5 +5,8 @@ use App\Utils\Router;
 require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config/config.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $router = new Router();
 $router->run();
